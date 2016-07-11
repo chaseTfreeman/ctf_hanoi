eafc7a1c6ce30e72be3c34965676a6dc030e78ef
Chase <chasefreeman13@gmail.com>
2016-07-11 14:25:22 -0400

    basic drag & drop setup ;) BooYAA

--- script.js
+++ script.js
@@ -2,5 +2,34 @@ $("document").ready(function(){
   console.log("Ready!");
 
 
+  /*
+  SYNTAX : draggable() method can be used in two forms:
+
+  $(selector, context).draggable (options) Method
+
+  $(selector, context).draggable ("action", [params]) Method
+  */
+
+  $(".disc").draggable({
+    revert: "invalid",
+    //on Snap: add CSS class to center, etc.
+    snap: ".discContainer",
+    snapMode: "inner",
+    stack: ".disc",
+    over: function( event, ui ) {
+                $(ui.draggable).addClass('.disc');
+            },
+            out: function (event, ui) {
+                $(ui.draggable).removeClass('.disc');
+            }
+  });
+
+  $(".discContainer").droppable({
+    drop: function(event, ui) {
+      console.log("Dropped!")
+
+    }
+
+  });
 
 });
