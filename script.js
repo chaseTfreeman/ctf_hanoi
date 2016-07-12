e946c5ad37c67e9f94b198daf72ae8d8e41faefe
Chase <chasefreeman13@gmail.com>
2016-07-12 11:17:53 -0400

    On drop append works-style issue tho

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
 $("document").ready(function(){
   console.log("Ready!");
 
-
-//***********Begin Drag & Drop Code:******************
+  //***********Begin Drag & Drop Code:******************
   $(".disc").draggable({
     revert: "invalid",
     snap: ".discContainer",
     snapMode: "inner",
     stack: ".disc",
-
   });
-
   $(".discContainer").droppable({
     drop: function(event, ui) {
-      console.log("Dropped!")
+      ($(this)).append($(ui.draggable)),
+      ui.draggable.addClass('dropped');
+      /*refresh positions
+      var refreshPositions = $(".disc").draggable( "option", "refreshPositions" );
+      $( ".disc" ).draggable( "option", "refreshPositions", true );
+      */
+      console.log($(this))
     }
-
-
   });
-//***********End Drag & Drop Code**************
+  //***********End Drag & Drop Code**************
 
-//************Handle the addClass when moving between Divs**********
-  // function init() {
-  //   $('.disc').draggable();
-  //   $('.discContainer').droppable( {
-  //     drop: handleDropEvent
-  //   } );
-  // }
-  // function handleDropEvent( event, ui ) {
-  //   $("#disc").removeClass(".disc").addClass(".dropped");
-  //   console.log("Class Change")
-  // }
+  //************Handle the addClass when moving between Divs**********
+
   //**********End addClass code*********************
 });
