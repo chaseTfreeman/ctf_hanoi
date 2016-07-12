62075ca7a1e78ab450d5ab6bbdd3eefab6d4ade7
Chase <chasefreeman13@gmail.com>
2016-07-12 12:21:53 -0400

    abandoning DnD

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 $("document").ready(function(){
   console.log("Ready!");
 
-  //***********Begin Drag & Drop Code:******************
-  $(".disc").draggable({
-    revert: "invalid",
-    snap: ".discContainer",
-    snapMode: "inner",
-    stack: ".disc",
-  });
-  $(".discContainer").droppable({
-    drop: function(event, ui) {
-      ($(this)).append($(ui.draggable)),
-      ui.draggable.addClass('dropped');
-      /*refresh positions
-      var refreshPositions = $(".disc").draggable( "option", "refreshPositions" );
-      $( ".disc" ).draggable( "option", "refreshPositions", true );
-      */
-      console.log($(this))
-    }
-  });
+
+
+
+
+
+
+
+
+  
+  //
+  // //***********Begin Drag & Drop Code:******************
+  // $(".disc").draggable({
+  //   revert: "invalid",
+  //   // snap: ".discContainer",
+  //   // snapMode: "inner",
+  //   stack: ".disc",
+  //
+  // });
+  // $(".discContainer").droppable({
+  //   drop: function(event, ui) {
+  //     console.log($(this));
+  //    ($(this)).append($(ui.draggable)),
+  //     //ui.draggable.addClass('dropped');
+  //     /*refresh positions
+  //     var refreshPositions = $(".disc").draggable( "option", "refreshPositions" );
+  //     $( ".disc" ).draggable( "option", "refreshPositions", true );
+  //     */
+  //     console.log($(this))
+  //   }
+  // });
   //***********End Drag & Drop Code**************
 
   //************Handle the addClass when moving between Divs**********
