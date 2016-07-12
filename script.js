51953f0ae08bf40048c19ea5cb1f806f558dd2a3
Chase <chasefreeman13@gmail.com>
2016-07-12 19:08:25 -0400

    everything works so far-- nothing a few beers can't do ;)

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 $("document").ready(function(){
   console.log("Ready!");
-  //***********Begin Drag & Drop Code:******************
-  $(".disc").draggable({
-    revert: "invalid",
-    snap: ".discContainer",
-    snapMode: "inner",
-    stack: ".disc",
-    helper: 'clone',
-    opacity: 0.35,
-  });
-  $(".discContainer").droppable({
-    drop: function(event, ui) {
-      ($(this)).prepend($(ui.draggable)),
-      console.log($(this))
-    }
-  });
-  //***********End Drag & Drop Code**************
+
   //*********** Start Dynamic Disk Creation************
   var numDiscs
   function getDiscNum() {
@@ -32,15 +17,34 @@ $("document").ready(function(){
   function createDisc(num) {
     num = parseInt(num);
     for(i = 1; i != num+1; i++){
-      $("#discContainerA").prepend("<div class=disc draggable=true" +" " + "id=" + i + ">").css('height', "30")
+      $("#discContainerA").prepend("<div class='disc' draggable='true' ui-draggable" +" " + "id=" + i + ">").css('height', "30")
       $('.disc').css('width', function(i) {
         return 100 + (i * 7) ;
       });
 
     }
-  }
+    //***********Begin Drag & Drop Code:******************
+    $(".disc").draggable({
+      revert: "invalid",
+      snap: ".discContainer",
+      snapMode: "inner",
+      stack: ".disc",
+      helper: 'clone',
+      opacity: 0.35,
+    });
+    $(".discContainer").droppable({
+      drop: function(event, ui) {
+        ($(this)).prepend($(ui.draggable)),
+        $(".disc").css("height", "10");
 
+
+        console.log($(this))
+      }
+    });
+    //***********End Drag & Drop Code**************
+  }
   //************END Dynamic Disk Creation******************
+
   //                      *********Rules*************
   //If discContainer's first child element has LOWER 'rank' than 'ui.draggable'
 
