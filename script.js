10e74792d86a83773d11a8be27ff20da092a5551
Chase <chasefreeman13@gmail.com>
2016-07-12 15:00:28 -0400

    Got DnD to work ;)--adding rules etc

--- script.js
+++ script.js
@@ -1,39 +1,33 @@
 $("document").ready(function(){
   console.log("Ready!");
-
-
-
-
-
-
-
-
-
-  
-  //
-  // //***********Begin Drag & Drop Code:******************
-  // $(".disc").draggable({
-  //   revert: "invalid",
-  //   // snap: ".discContainer",
-  //   // snapMode: "inner",
-  //   stack: ".disc",
-  //
-  // });
-  // $(".discContainer").droppable({
-  //   drop: function(event, ui) {
-  //     console.log($(this));
-  //    ($(this)).append($(ui.draggable)),
-  //     //ui.draggable.addClass('dropped');
-  //     /*refresh positions
-  //     var refreshPositions = $(".disc").draggable( "option", "refreshPositions" );
-  //     $( ".disc" ).draggable( "option", "refreshPositions", true );
-  //     */
-  //     console.log($(this))
-  //   }
-  // });
+  //***********Begin Drag & Drop Code:******************
+  $(".disc").draggable({
+    revert: "invalid",
+    snap: ".discContainer",
+    snapMode: "inner",
+    stack: ".disc",
+    helper: 'clone',
+    opacity: 0.35,
+  });
+  $(".discContainer").droppable({
+    drop: function(event, ui) {
+      ($(this)).prepend($(ui.draggable)),
+      console.log($(this))
+    }
+  });
   //***********End Drag & Drop Code**************
-
-  //************Handle the addClass when moving between Divs**********
+  //*********** Start Dynamic Disk Creation************
+  $("#target").submit(function( event ) {
+    var numDiscs = $(".discChoice").val()
+    event.preventDefault();
+     for (i = 0; i < numDiscs; i++) {
+       console.log(numDiscs);
+       }
+     });
+  //************END Dynamic Disk Creation******************
+  //                      *********Rules*************
+  //If discContainer's first child element has LOWER 'rank' than 'ui.draggable'
+  // then revert/cancel the drag/drop.
 
   //**********End addClass code*********************
 });
